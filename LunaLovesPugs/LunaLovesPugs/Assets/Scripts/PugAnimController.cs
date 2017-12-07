0529dd0a1077eca128b097861b9a342c8dd4541b
McKenna <mbykowski0413@gmail.com>
2017-12-07 16:23:28 -0500

    Finished pug animations

--- LunaLovesPugs/LunaLovesPugs/Assets/Scripts/PugAnimController.cs
+++ LunaLovesPugs/LunaLovesPugs/Assets/Scripts/PugAnimController.cs
@@ -31,7 +31,7 @@ public class PugAnimController : MonoBehaviour {
 		if (Input.GetButtonDown("Jump") && isGrounded)
 		{
 			jump = true;
-			anim.SetTrigger("Jump");
+			//anim.SetTrigger("Jump");
 		}
 	}
 
